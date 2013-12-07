5c039a5117741bace4578c8b93bf7508707fe713
fluorine <josephmail7@gmail.com>
2013-12-06 22:46:21 -0400

    HelloWorld typical program.

---
+++ HelloWorld/src/com/github/fluorine/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.github.fluorine.helloworld;
+
+/**
+ * Typical HelloWorld program.
+ * 
+ * @author fluorine@github.com
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
